47d33cc2566328d2a6f9126bbe5ac42517e2a00a
abuelitovip <43369593+Abuelitovip@users.noreply.github.com>
2022-06-18 12:57:45 +0200

    new filter for "feature" option in url
    fix black screen in videos with "feature" text in url.

--- contentScript.js
+++ contentScript.js
@@ -4,9 +4,9 @@
     ];
     let destino = window.location.href;// url a comprobar    
     
-    if(destino.startsWith(casos[0]) || destino.startsWith(casos[1])){        
+    if(destino.startsWith(casos[0]) || destino.startsWith(casos[1])){     
         destino = destino.replace("youtu.be/","www.youtube.com/embed/");
-        destino = destino.replace("&feature=youtu.be","");
+        destino = destino.split("&feature")[0];
         destino = destino.replace("/watch?v=","/embed/");//change to semi fullscreen
         destino = destino.replace("&t=","?start=");//change time start of video
         destino = destino.replace("&start=","?start=");//change time start of video
@@ -19,5 +19,9 @@
         destino = destino.replace("&feature=youtu.be","");
         destino = destino.replace("&feature=emb_logo","");
         window.location.replace(destino);
+    }
+    else if(destino.startsWith("https://www.youtube.com/embed") && destino.includes("&feature")){
+        destino = destino.split("&feature")[0]
+        window.location.replace(destino)
     }
 
